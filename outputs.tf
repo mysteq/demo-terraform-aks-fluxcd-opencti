04e7a8eefd7a9b6543967d8b7abd92c51f961d4d
Ketil Gjerde <raideer@gmail.com>
2023-06-01 13:50:40 +0200

    chore: demo test
    Signed-off-by: Ketil Gjerde <raideer@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "oidc_issuer_url" {
 output "user_assigned_identity_clientid" {
   value = "${azurerm_user_assigned_identity.demo.client_id}"
 }
+
+output "key_vault_url" {
+  value = "${azurerm_key_vault.demo.vault_uri}"
+}
